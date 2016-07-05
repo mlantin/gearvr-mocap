5b1976341391600557fa87cdb93bac53b02bd28e
Maria Lantin <maria.lantin@gmail.com>
2016-07-04 18:22:27 -0700

    still trying to find a good way to deal with the drift. getting close but not quite there yet.

--- Assets/Roaming.cs
+++ Assets/Roaming.cs
@@ -1,14 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Roaming : MonoBehaviour {
 
 	public GameObject worldOrigin;
+	public Text debugTxt;
 
 	Vector3 newpos = new Vector3();
 	Quaternion newrot = new Quaternion();
 	OVRPose headpos = new OVRPose();
 	bool havemocap = false;
+	bool trackingspaceSet = false;
 
 	void Awake() {
 		var rig = GameObject.FindObjectOfType<OVRCameraRig>();
@@ -19,21 +22,26 @@ public class Roaming : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		SocketDispatch.On ("GVR", handleMocap);
+		SocketDispatch.On ("GVR2", handleMocap);
 	}
 
 
 	void UpdatedAnchors(OVRCameraRig r) {
 		Quaternion hmd_rotation = Quaternion.identity;
-		if (havemocap) {
+		if (havemocap && !trackingspaceSet) {
 			// get the difference between the hmd rotation and the world origin rotation.
+			OVRPlugin.Quatf hmd_origin = OVRPlugin.GetTrackingCalibratedOrigin().Orientation;
+			Quaternion hmdOrigin = new Quaternion (hmd_origin.x, hmd_origin.y, hmd_origin.z, hmd_origin.w);
+			debugTxt.text = newrot.eulerAngles.ToString ();
 			hmd_rotation = r.centerEyeAnchor.ToOVRPose (true).orientation;
-			Quaternion rotdiff = Quaternion.Inverse (newrot) * hmd_rotation;
+			Quaternion rotdiff = Quaternion.Inverse (newrot)*hmdOrigin;
+
 			// now slerp the world origin to make up for the difference between the hmd and mocap orientation
 			// at the start this will mean a slight delay in setting the world orientation because there will be
 			// a significant difference between the mocap and the hmd. Maybe 1 or 2 seconds.
 			// could also try using the rotateToward function.
-			worldOrigin.transform.rotation = Quaternion.Slerp (worldOrigin.transform.rotation, rotdiff, Time.deltaTime);
+			worldOrigin.transform.rotation = rotdiff;
+			trackingspaceSet = true;
 		}
 
 
@@ -44,13 +52,14 @@ public class Roaming : MonoBehaviour {
 		headpos.position = newpos;
 		//r.trackingSpace.FromOVRPose(headpos);
 //		transform.rotation = headpos.orientation;
-		transform.position = headpos.position;
+		transform.position = worldOrigin.transform.rotation * headpos.position;
 	}
 
 	// Update is called once per frame
 	void Update () {
 		//transform.position = newpos;
 		//transform.localRotation = newrot;
+		debugTxt.text = newrot.eulerAngles.ToString ();
 	}
 
 	void handleMocap(Google.Protobuf.VRCom.MocapSubject head) {
