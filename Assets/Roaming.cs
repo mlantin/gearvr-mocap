1475d541afb657ada5a3a851b3c6a8f748d6ba42
Maria Lantin <maria.lantin@gmail.com>
2016-07-05 19:59:56 -0700

    drift issue is now mostly solved...there could be some optimization based on considering the mocap lag time. But basically it adjusts the world orientation gradually to match the gearvr.

--- Assets/Roaming.cs
+++ Assets/Roaming.cs
@@ -5,17 +5,14 @@ using UnityEngine.UI;
 public class Roaming : MonoBehaviour {
 
 	public GameObject worldOrigin;
-	public Text debugTxt;
 
 	Vector3 newpos = new Vector3();
 	Quaternion newrot = new Quaternion();
-	OVRPose headpos = new OVRPose();
 	bool havemocap = false;
 	bool trackingspaceSet = false;
 
 	void Awake() {
 		var rig = GameObject.FindObjectOfType<OVRCameraRig>();
-		//rig.UpdatedAnchors += r => r.trackingSpace.FromOVRPose(headpos*rig.centerEyeAnchor.ToOVRPose(true).Inverse());
 		rig.UpdatedAnchors += UpdatedAnchors;
 
 	}
@@ -28,38 +25,32 @@ public class Roaming : MonoBehaviour {
 
 	void UpdatedAnchors(OVRCameraRig r) {
 		Quaternion hmd_rotation = Quaternion.identity;
-		if (havemocap && !trackingspaceSet) {
+		if (havemocap) {
 			// get the difference between the hmd rotation and the world origin rotation.
-			OVRPlugin.Quatf hmd_origin = OVRPlugin.GetTrackingCalibratedOrigin().Orientation;
-			Quaternion hmdOrigin = new Quaternion (hmd_origin.x, hmd_origin.y, hmd_origin.z, hmd_origin.w);
-			debugTxt.text = newrot.eulerAngles.ToString ();
 			hmd_rotation = r.centerEyeAnchor.ToOVRPose (true).orientation;
-			Quaternion rotdiff = Quaternion.Inverse (newrot)*hmdOrigin;
-
-			// now slerp the world origin to make up for the difference between the hmd and mocap orientation
-			// at the start this will mean a slight delay in setting the world orientation because there will be
-			// a significant difference between the mocap and the hmd. Maybe 1 or 2 seconds.
-			// could also try using the rotateToward function.
-			worldOrigin.transform.rotation = rotdiff;
-			trackingspaceSet = true;
+			Quaternion rotdiff = hmd_rotation*Quaternion.Inverse (newrot);
+		
+			// if we're just starting up, set the world orientation to match the gearvr
+			if (!trackingspaceSet) {
+				worldOrigin.transform.rotation = rotdiff;
+				trackingspaceSet = true;
+			} else {
+				// otherwise adjust the world orientation up to .01 of a degree. Basically this will drift the world
+				// orientation in tandem with the gearvr. Seems to work. I'm sure the value of .01 could be tweaked
+				// to be optimal. The issue is that you don't want to have too large because then it can shift wildly
+				// because of the mocap lag. There may be a way to check for moments of relative quiet and measure
+				// the drift. In practice this code works ok but it does mean that the world is always a bit shifty
+				// which probably induces nausea.
+				worldOrigin.transform.rotation = Quaternion.RotateTowards (worldOrigin.transform.rotation, rotdiff, .01f);
+			}
 		}
-
-
-//		Quaternion oldOrientation = headpos.orientation;
-//		hmd_rotation = r.centerEyeAnchor.ToOVRPose (true).orientation;
-		//hmd_rotation = OVRManager.display.GetHeadPose(0).orientation;
-//		headpos.orientation = Quaternion.Slerp(oldOrientation, newrot * Quaternion.Inverse(hmd_rotation), Time.deltaTime);
-		headpos.position = newpos;
-		//r.trackingSpace.FromOVRPose(headpos);
-//		transform.rotation = headpos.orientation;
-		transform.position = worldOrigin.transform.rotation * headpos.position;
+			
+		transform.position = worldOrigin.transform.rotation * newpos;
 	}
 
 	// Update is called once per frame
 	void Update () {
-		//transform.position = newpos;
-		//transform.localRotation = newrot;
-		debugTxt.text = newrot.eulerAngles.ToString ();
+
 	}
 
 	void handleMocap(Google.Protobuf.VRCom.MocapSubject head) {
